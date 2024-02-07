91bcf69c34e194917c59016c61fce6f1fe379038
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-02-07 09:22:32 +0100

    edit-mat

--- Beautiful Matrix.cpp
+++ Beautiful Matrix.cpp
@@ -1,52 +1,52 @@
 #include <iostream>
-
+ 
 using namespace std;
-
+ 
 int main()
 {
-
-int n,a,b,s=0,i,j,cpt=0;
-
-int m[5][5];
-
+ 
+ int row_index, col_index, swap_count = 0,minimum_number_of_moves=0;
+ 
+int array2d[5][5];
+ 
 for(size_t i=0;i<5;i++){
         for(size_t j=0;j<5;j++){
           
-                cin>>m[i][j];
+                cin>>array2d[i][j];
         }
 }
             
     for(size_t i=0;i<5;i++){
         for(size_t j=0;j<5;j++){
-               if(m[i][j]==1){
-                    a=i;
-                    b=j;
+               if(array2d[i][j]==1){
+                    row_index=i;
+                    col_index=j;
                     while(j!=2){
                        if(j>2)
                   j=j-1;
                else   
                 j=j+1;
-                cpt++;
+                minimum_number_of_moves++;
                     }
                     while(i!=2){
                        if(i>2)
                             i--;
                        else
                         i++;
-                       cpt=cpt+1;
-
+                       minimum_number_of_moves=minimum_number_of_moves+1;
+ 
                     }
-                    s=m[a][b];
-                    m[a][b]=m[i][j];
-                    m[i][j]=s;
-
-
+                    swap_count=array2d[row_index][col_index];
+                    array2d[row_index][col_index]=array2d[i][j];
+                    array2d[i][j]=swap_count;
+ 
+ 
                }
-
-
+ 
+ 
         }
     }
-
+ 
 cout<<"\n";
-  cout<<cpt;
+  cout<<minimum_number_of_moves;
 }
