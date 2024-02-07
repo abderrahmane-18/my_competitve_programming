b8f3c94b92343fa2578b38a565e5af6f91cd23db
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-02-07 09:03:25 +0100

    edit-exp

--- Expression.cpp
+++ Expression.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main()
-{int a,b,c,m1,m2,m3,m4,max,m5;
+{int positive_number1,positive_number2,positive_number3,max1,max2,max3,max4,max,max5;
 int arr[5];
-    cin >> a;
-    cin >> b;
-    cin >> c;
+    cin >> positive_number1;
+    cin >> positive_number2;
+    cin >> positive_number3;
     
- m1=a+b*c;
-m2=a*(b+c);
-m3=a*b*c;
-m4=(a+b)*c;
-m5=a+b+c;
+ max1=positive_number1+positive_number2*positive_number3;
+max2=positive_number1*(positive_number2+positive_number3);
+max3=positive_number1*positive_number2*positive_number3;
+max4=(positive_number1+positive_number2)*positive_number3;
+max5=positive_number1+positive_number2+positive_number3;
 for(size_t i=0;i<5;i++){
-    arr[0]=m1;
-    arr[1]=m2;
-    arr[2]=m3;
-    arr[3]=m4;
-    arr[4]=m5;
+    arr[0]=max1;
+    arr[1]=max2;
+    arr[2]=max3;
+    arr[3]=max4;
+    arr[4]=max5;
 }
-max=m1;
+max=max1;
 for(size_t i=1;i<5;i++){
     if(arr[i]>=max)
     max=arr[i];
